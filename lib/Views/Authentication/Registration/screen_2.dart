ff9efefa1fa3c914e5d0facc4808f6202818daff
mimi-tech <onuohamiriam44@gmail.com>
2022-10-19 10:11:16 +0100

    Added txn screen

--- lib/Views/Authentication/Registration/screen_2.dart
+++ lib/Views/Authentication/Registration/screen_2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pinput/pinput.dart';
 import 'package:sure_move/Commons/colors.dart';
 import 'package:sure_move/Commons/constants.dart';
 import 'package:sure_move/Commons/dimens.dart';
@@ -21,7 +22,47 @@ class _RegistrationScreen2State extends State<RegistrationScreen2> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String? referral;
   Gender _gender = Gender.male;
+  final TextEditingController _pinPutController = TextEditingController();
+  final FocusNode _pinPutFocusNode = FocusNode();
+  final defaultPinTheme = PinTheme(
+    width: 56,
+    height: 56,
+    textStyle: const TextStyle(fontSize: 20, color:kTextColor, fontWeight: FontWeight.w600),
 
+    decoration: BoxDecoration(
+      border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+      borderRadius: BorderRadius.circular(20),
+    ),
+  );
+
+  Widget animatingBorders() {
+
+    return Pinput(
+
+      autofocus: true,
+      //validator: Variables.validatePin,
+      obscureText: true,
+      obscuringCharacter: "*",
+      keyboardType:TextInputType.number,
+      length: 6,
+      focusNode: _pinPutFocusNode,
+      controller: _pinPutController,
+      defaultPinTheme: defaultPinTheme,
+      focusedPinTheme: PinTheme(
+        decoration: BoxDecoration(
+          border: Border.all(color: kOrangeColor),
+          borderRadius: BorderRadius.circular(8),
+        )
+      ),
+      submittedPinTheme: PinTheme(
+        decoration: BoxDecoration(
+          border: Border.all(color: kGreen),
+          borderRadius: BorderRadius.circular(8),
+        )
+      ),
+      pinAnimationType: PinAnimationType.slide,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +84,8 @@ class _RegistrationScreen2State extends State<RegistrationScreen2> {
                       crossAxisAlignment:CrossAxisAlignment.start,
                       children: [
                         spacing(),
-
+                        animatingBorders(),
+                        spacing(),
                         TextFormField(
                           controller: _referral,
                           autocorrect: true,
